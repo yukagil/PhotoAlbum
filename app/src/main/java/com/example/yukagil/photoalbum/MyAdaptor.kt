482249743d17220fe7cfe712f4c1873337ededb8
yukagil <litandy.ky@gmail.com>
2018-10-10 00:45:13 +0900

    RecyclerViewに任意のImageを表示する

--- app/src/main/java/com/example/yukagil/photoalbum/MyAdaptor.kt
+++ app/src/main/java/com/example/yukagil/photoalbum/MyAdaptor.kt
@@ -4,9 +4,25 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class MyAdaptor : RecyclerView.Adapter<MyAdaptor.MyViewHolder>() {
+
+    private val imageUrls = arrayOf(
+            "https://picsum.photos/600/400?image=500",
+            "https://picsum.photos/600/400?image=501",
+            "https://picsum.photos/600/400?image=502",
+            "https://picsum.photos/600/400?image=503",
+            "https://picsum.photos/600/400?image=504",
+            "https://picsum.photos/600/400?image=505",
+            "https://picsum.photos/600/400?image=506",
+            "https://picsum.photos/600/400?image=507",
+            "https://picsum.photos/600/400?image=508",
+            "https://picsum.photos/600/400?image=509",
+            "https://picsum.photos/600/400?image=510"
+    )
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_cell, parent, false)
@@ -14,13 +30,15 @@ class MyAdaptor : RecyclerView.Adapter<MyAdaptor.MyViewHolder>() {
         return MyViewHolder(view)
     }
 
-    override fun getItemCount() = 100
+    override fun getItemCount() = imageUrls.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.itemCell.text = position.toString()
+        Picasso.with(holder.itemCell.context)
+                .load(imageUrls[position])
+                .into(holder.itemCell)
     }
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val itemCell: TextView = view.findViewById(R.id.itemCell)
+        val itemCell: ImageView = view.findViewById(R.id.itemCell)
     }
 }
