68e7761ec7567996f101ac1ca38ceb9b070e67b0
yukagil <litandy.ky@gmail.com>
2018-10-19 23:33:59 +0900

    ビューアにshareと作者ページへ遷移するメニューを追加

--- app/src/main/java/com/example/yukagil/photoalbum/PhotoViewer.kt
+++ app/src/main/java/com/example/yukagil/photoalbum/PhotoViewer.kt
@@ -1,19 +1,50 @@
 package com.example.yukagil.photoalbum
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_photo_viewer.*
 
 class PhotoViewer : AppCompatActivity() {
 
+    private lateinit var imageObject: Image
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_viewer)
 
-        val imageObject = intent.getParcelableExtra<Image>("imageObject")
+        imageObject = intent.getParcelableExtra<Image>("imageObject")
         Picasso.with(this)
                 .load("https://picsum.photos/600/400?image=${imageObject.id}")
                 .into(imageView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_photo_viewer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.share -> {
+                val intent = Intent(Intent.ACTION_SEND)
+                        .setType("image/png")
+                        .putExtra(Intent.EXTRA_TEXT, imageObject.author_url)
+                startActivity(intent)
+
+                true
+            }
+            R.id.author_info -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(imageObject.author_url))
+                startActivity(intent)
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
